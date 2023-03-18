75d9ffd2ebe01021ff34107720b656a37461c90c
shamith01 <128206892+shamith01@users.noreply.github.com>
2023-03-18 11:54:47 -0700

    hi

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("monitor1").addEventListener("click",function() {
+    window.location="https://www.amazon.in/Lenovo-68-58cm-FreeSync-Speakers-Certified/dp/B0BLSNNLV3/ref=sr_1_5?crid=3TK5V13RAPWIO&keywords=innocn+curved+monitor&qid=1677390568&sprefix=innocn+curved+monitor%2Caps%2C206&sr=8-5";
+    window.focus();
+
+})
+
+document.getElementById("laptop1").addEventListener("click",function() {
+    window.location="https://www.amazon.in/Acer-i5-11400H-Processor-Graphics-AN515-57/dp/B09X7SXPKC/ref=sr_1_1_sspa?crid=11NLALQCF6SOJ&keywords=acer+nitro+5&qid=1677390794&sprefix=acer+nitro%2Caps%2C200&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1";
+    window.focus();
+
+})
+document.getElementById("headphone1").addEventListener("click",function() {
+    window.location="https://www.amazon.in/Cosmic-Byte-Spider-Microphone-Red/dp/B081D4XWWB/ref=sr_1_2?crid=29AOTOX89EWKE&keywords=cosmic+byte+spider+gaming+headphone&qid=1677390995&sprefix=cosmic+byte+spider%2Caps%2C211&sr=8-2";
+    window.focus();
+
+})
